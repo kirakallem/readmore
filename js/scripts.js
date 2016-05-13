bfe6653ad367228c1214156ec7aa4775732f2086
kirakallem <kira.kallem@gmail.com>
2016-05-13 15:58:43 -0700

    Tubular insert

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 // My Scripts
 
-$('article').readmore({
-  
-  speed: 2000,
-  collapsedHeight: 100,
-  moreLink: '<a href="#">Showwyyyy</a>',
-  lessLink: '<a href="#">Hiddddyyyy</a>',
-  
+$('document').ready(function() {
+  var options = { videoId: 'dCUwDIWNxew', start: 3 };
+	$('#wrapper').tubular(options);
+	//ratio: 16/9 // usually either 4/3 or 16/9 -- tweak as needed
+//videoId: 'ZCAnLxRvNNc' // youTube video id
+//mute: true
+//repeat: true
+//wrapperZIndex: 99
+//playButtonClass: 'tubular-play'
+//pauseButtonClass: 'tubular-pause'
+//muteButtonClass: 'tubular-mute'
+//volumeUpClass: 'tubular-volume-up'
+//volumeDownClass: 'tubular-volume-down'
+//increaseVolumeBy: 10 // increment value; volume range is 1-100
+//start: 0 // starting position in seconds
 });
